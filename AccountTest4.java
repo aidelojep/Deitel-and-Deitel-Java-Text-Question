cbfdbff05377c15dfc84df698cb5aa499f59e8c4
aidelojep <57565189+aidelojep@users.noreply.github.com>
2021-02-18 20:44:14 +0100

    Add files via upload

---
+++ AccountTest4.java
@@ -0,0 +1,34 @@
+ 
+
+   public class AccountTest4 {
+
+     public static void main(String[] args) {
+     
+       
+         Account4 peter = new Account4("Jane Green", 40.00 );
+         
+
+             System.out.printf("Name, %s %n" , peter.getName());
+
+             System.out.printf("Balance, %s %n", peter.getBalance());
+              
+              peter.deposit(50.00);
+             System.out.printf("Current balance is  %f%n", peter.getBalance());
+
+     
+
+                
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
